6ceee6443fc7990a6d9594299771564490975fcd
Chetan <chetan17302@gmail.com>
2024-03-28 19:56:01 +0530

    Added project section

---
+++ src/app/projects/projectdata.ts
@@ -0,0 +1,51 @@
+export const projects = [
+    {
+        projectname:"NotesApp",
+        projectlinks:{
+            github:"https://github.com/Chetan-1-hash/NotesApp",
+            demo:null,
+        },
+        projectimage:"/assets/Noteapp-img.png",
+        projectdescription:"NotesApp having features like CRUD Operations and some UI features",
+        projectskill:[
+            "Angular17", "Bootstrap", "Spring Boot", "MongoDB"
+        ],
+    },
+    {
+        projectname:"WeatherApp",
+        projectlinks:{
+            github:"https://github.com/Chetan-1-hash/WeatherApp",
+            demo:null,
+        },
+        projectimage:"/assets/Weatherapp-img.png",
+        projectdescription:"WeatherApp show the weather for particular location",
+        projectskill:[
+            "Angular17", "Openweather Api"
+        ],
+    },
+    {
+        projectname:"Myntra Clone app",
+        projectlinks:{
+            github:null,
+            demo:null,
+        },
+        projectimage:"/assets/Myntraclone-img.png",
+        projectdescription:"Myntraclone app cloned and  having some additional collaboration features.",
+        projectskill:[
+            "Angular17",
+        ],
+    },
+    {
+        projectname:"WeatherApp",
+        projectlinks:{
+            github:null,
+            demo:null,
+        },
+        projectimage:"/assets/OnlineExamSystem-img.png",
+        projectdescription:"WeatherApp created using Angular 17,",
+        projectskill:[
+            "Angular17", "Bootstrap", "Spring Boot", "MySQL",
+        ],
+    },
+    
+]
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { projects } from './projectdata';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
+
+  projectdata = projects;
+  gittooltiptext!: string;
+  demotooltiptext!: string;
+
+
+  opentab(i: number) {
+    const githubUrl = this.projectdata[i].projectlinks.github;
+    if (githubUrl) {
+      window.open(githubUrl, '_blank');
+    }
+    else {
+      console.log("ERROR at url")
+    }
+  }
+
+  showGitToolTip(i: number) {
+    const githubUrl = this.projectdata[i].projectlinks.github;
+    if (githubUrl) {
+      this.gittooltiptext = "Source Code"
+    }
+    else {
+      this.gittooltiptext = "No Code";
+    }
+  }
+
+  showDemoToolTip(i: number) {
+    const demoUrl = this.projectdata[i].projectlinks.demo;
+    if (demoUrl) {
+      this.demotooltiptext = "Live Demo"
+    }
+    else {
+      this.demotooltiptext = "No Demo";
+    }
+  }
 
 }
