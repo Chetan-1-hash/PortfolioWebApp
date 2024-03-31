3415fef1e950af4f7421bbea6bb6a32246e9ed33
Chetan <chetan17302@gmail.com>
2024-03-31 15:26:19 +0530

    Added auto-text effect

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,63 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faGithub, faHackerrank, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+  import { Observable, interval } from 'rxjs';
+  import { map, takeWhile } from 'rxjs/operators';
+  import { Component, ElementRef, ViewChild } from '@angular/core';
+  import { FormsModule } from '@angular/forms';
+  import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+  import { faGithub, faHackerrank, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+  import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-@Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [FontAwesomeModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
-})
-export class HomeComponent {
-  faEmail = faEnvelope;
-  faLinkedIn = faLinkedin;
-  faInsta = faInstagram;
-  faHackerRank = faHackerrank;
-  faGithub = faGithub;
-}
+  @Component({
+    selector: 'app-home',
+    standalone: true,
+    imports: [FontAwesomeModule],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css'
+  })
+  export class HomeComponent {
+    faEmail = faEnvelope;
+    faLinkedIn = faLinkedin;
+    faInsta = faInstagram;
+    faHackerRank = faHackerrank;
+    faGithub = faGithub;
+
+    text: string = "Web Developer";
+    typedText = "";
+
+    @ViewChild('designation') designation!: ElementRef;
+
+    ngOnInit() {
+      this.writeText();
+    }
+
+    writeText() {
+      const speed = 300;
+      let idx = 0;
+      let direction = 1;
+  
+      const type = () => {
+        if (direction === 1) {
+          if (idx < this.text.length) {
+            this.typedText = this.text.slice(0, idx + 1);
+            idx++;
+          } 
+          else {
+            direction = -1; // Change direction to reverse
+          }
+        } 
+        else {
+          if (idx > 0) {
+            this.typedText = this.text.slice(0, idx);
+            idx--;
+          } 
+          else {
+            direction = 1; // Change direction to forward
+          }
+        }
+  
+        setTimeout(type, speed); // Call type function recursively after a delay
+      };
+  
+      type(); // Start the typing animation
+    }
+    
+  }
