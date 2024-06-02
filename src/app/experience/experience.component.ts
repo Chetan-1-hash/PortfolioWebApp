d683448d9c6240aa9e16c7ceda136185491346e7
Chetan <chetan17302@gmail.com>
2024-06-02 17:07:16 +0530

    Updated alignments

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowAltCircleRight, faArrowCircleRight, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
 })
 export class ExperienceComponent {
-
+  arrow = faArrowCircleRight
 }
