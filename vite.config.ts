d089a02d583417fc93def7c9f91e5e00a62cac0e
Chetan <chetan17302@gmail.com>
2026-02-07 08:58:52 +0530

    Fixing error-5

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  base: '/PortfolioWebApp/',
+export default defineConfig({
+  // Hardcode the base to your repo name
+  base: "/PortfolioWebApp/", 
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  build: {
+    outDir: 'dist',
+  }
+});
